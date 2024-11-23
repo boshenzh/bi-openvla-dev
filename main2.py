896ba7d4bc2571e30118a6a0be036b0fa5488b8d
Yimin Tang <tangym@shanghaitech.edu.cn>
2024-11-22 23:21:36 -0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -58,6 +58,92 @@ def resize_image(img, resize_size):
     img = img.numpy()
     return img
 
+def image_preprocess(image, center_crop=True):
+    image = Image.fromarray(image)
+    image = image.convert("RGB")
+
+    # (If trained with image augmentations) Center crop image and then resize back up to original size.
+    # IMPORTANT: Let's say crop scale == 0.9. To get the new height and width (post-crop), multiply
+    #            the original height and width by sqrt(0.9) -- not 0.9!
+    if center_crop:
+        batch_size = 1
+        crop_scale = 0.9
+
+        # Convert to TF Tensor and record original data type (should be tf.uint8)
+        image = tf.convert_to_tensor(np.array(image))
+        orig_dtype = image.dtype
+
+        # Convert to data type tf.float32 and values between [0,1]
+        image = tf.image.convert_image_dtype(image, tf.float32)
+
+        # Crop and then resize back to original size
+        image = crop_and_resize(image, crop_scale, batch_size)
+
+        # Convert back to original data type
+        image = tf.clip_by_value(image, 0, 1)
+        image = tf.image.convert_image_dtype(image, orig_dtype, saturate=True)
+
+        # Convert back to PIL Image
+        image = Image.fromarray(image.numpy())
+        image = image.convert("RGB")
+    return image
+
+def crop_and_resize(image, crop_scale, batch_size):
+    """
+    Center-crops an image to have area `crop_scale` * (original image area), and then resizes back
+    to original size. We use the same logic seen in the `dlimp` RLDS datasets wrapper to avoid
+    distribution shift at test time.
+
+    Args:
+        image: TF Tensor of shape (batch_size, H, W, C) or (H, W, C) and datatype tf.float32 with
+               values between [0,1].
+        crop_scale: The area of the center crop with respect to the original image.
+        batch_size: Batch size.
+    """
+    # Convert from 3D Tensor (H, W, C) to 4D Tensor (batch_size, H, W, C)
+    assert image.shape.ndims == 3 or image.shape.ndims == 4
+    expanded_dims = False
+    if image.shape.ndims == 3:
+        image = tf.expand_dims(image, axis=0)
+        expanded_dims = True
+
+    # Get height and width of crop
+    new_heights = tf.reshape(tf.clip_by_value(tf.sqrt(crop_scale), 0, 1), shape=(batch_size,))
+    new_widths = tf.reshape(tf.clip_by_value(tf.sqrt(crop_scale), 0, 1), shape=(batch_size,))
+
+    # Get bounding box representing crop
+    height_offsets = (1 - new_heights) / 2
+    width_offsets = (1 - new_widths) / 2
+    bounding_boxes = tf.stack(
+        [
+            height_offsets,
+            width_offsets,
+            height_offsets + new_heights,
+            width_offsets + new_widths,
+        ],
+        axis=1,
+    )
+
+    # Crop and then resize back up
+    image = tf.image.crop_and_resize(image, bounding_boxes, tf.range(batch_size), (224, 224))
+
+    # Convert back to 3D Tensor (H, W, C)
+    if expanded_dims:
+        image = image[0]
+
+    return image
+
+def get_libero_image(obs, resize_size):
+    """Extracts image from observations and preprocesses it."""
+    assert isinstance(resize_size, int) or isinstance(resize_size, tuple)
+    if isinstance(resize_size, int):
+        resize_size = (resize_size, resize_size)
+    img = obs["agentview_image"]
+    img = img[::-1, ::-1]  # IMPORTANT: rotate 180 degrees to match train preprocessing
+    img = resize_image(img, resize_size)
+    return img
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -81,7 +167,7 @@ if __name__ == "__main__":
         low_cpu_mem_usage=True, 
         trust_remote_code=True
     ).to("cuda:0")
-    INSTRUCTION = "lift the box"
+    INSTRUCTION = "lift the red box"
     prompt = f"In: What action should the robot take to {INSTRUCTION}?\nOut:"
     
     controller_configs = robosuite.load_controller_config(default_controller="OSC_POSE")
@@ -115,25 +201,14 @@ if __name__ == "__main__":
 
     def policy(obs):
         # a trained policy could be used here, but we choose a random action
-        low, high = env.action_spec
-        img = np.flipud(np.fliplr(obs['robot0_robotview_image']))
-        camera_image0 = resize_image(img, (224, 224))
-        image1 = Image.fromarray(camera_image0)  # Convert the NumPy array to an image object
         
-        inputs1 = processor(prompt, image1).to("cuda:0", dtype=torch.bfloat16)
+        img = get_libero_image(obs, 224)
+        img = image_preprocess(img)
+        inputs1 = processor(prompt, img).to("cuda:0", dtype=torch.bfloat16)
         action1 = vla.predict_action(**inputs1, unnorm_key="libero_spatial", do_sample=False)
         action1 = normalize_gripper_action(action1)
-        
-        # img = np.flipud(np.fliplr(obs['robot1_robotview_image']))
-        # camera_image0 = resize_image(img, (224, 224))
-        # image1 = Image.fromarray(camera_image0)  # Convert the NumPy array to an image object
-        
-        # inputs1 = processor(prompt, image1).to("cuda:0", dtype=torch.bfloat16)
-        # action2 = vla.predict_action(**inputs1, unnorm_key="libero_spatial", do_sample=False)
-        # action2 = normalize_gripper_action(action1)
-        # action = np.concatenate([action1, action2])
-        
-        return action1
+        # action1 = invert_gripper_action(action1)
+        return action1, img
     
     obs = env.reset()
     print(obs.keys())
@@ -144,11 +219,13 @@ if __name__ == "__main__":
     output_file = os.path.join(args.directory, "output.mp4")
     writer = imageio.get_writer(output_file, fps=20)
     
+    output_file2 = os.path.join(args.directory, "output2.mp4")
+    writer2 = imageio.get_writer(output_file2, fps=20)
+    
     while not done:
-        action = policy(obs)         # use observation to decide on an action
+        action, img = policy(obs)         # use observation to decide on an action
+        
         obs, reward, done, _ = env.step(action) # play action
-        
-        
         grid_image = np.vstack([
             # 第一行：水平拼接 agentview 和 frontview 图像
             np.hstack([
@@ -161,7 +238,9 @@ if __name__ == "__main__":
                 np.flipud(np.fliplr(obs['robot0_robotview_image'])) # eye-in-hand 图像旋转 180 度
             ])
         ])
+        writer2.append_data(np.flipud(np.fliplr(img)))
         writer.append_data(grid_image)
         ret += reward
     writer.close()
+    writer2.close()
     print("rollout completed with return {}".format(ret))
